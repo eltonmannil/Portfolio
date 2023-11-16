12c142f272f54c8c46a37018007a0f35c00da533
Elton Mannil <106404339+eltonmannil@users.noreply.github.com>
2023-11-16 15:51:55 -0600

    Delete ConsoleApplication3 directory

--- ConsoleApplication3/degree.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <iostream>
-#include <string>
-
-//Define an enumerated data type DegreeProgram, data type values SECURITY, NETWORK and SOFTWARE
-enum DegreeProgram { SECURITY, NETWORK, SOFTWARE };
-
-static const std::string DegreeProgramStrings[] = { "SECURITY", "NETWORK", "SOFTWARE" };
